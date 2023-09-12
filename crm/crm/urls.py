d7b4155f36df8a4e240dd16b61745a5eaf3cf053
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-12 18:52:41 +0300

    User dashboard page

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import views
-from django.urls import path
+from django.urls import path, include
 from core.views import index, about
 from userprofile.views import signup
 
 urlpatterns = [
     path('', index, name='index'),
+    path('dashboard/', include('dashboard.urls')), # All urls that begin with dashboard will be handled by dashboard/urls.py
     path('about/', about, name='about'),
     path('signup/', signup, name='signup'),
     path('login/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
