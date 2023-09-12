6ce749da23687cb44e1cba5e41d0a6e7ca735e4c
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-12 19:02:43 +0300

    About page

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, about
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
 ]
