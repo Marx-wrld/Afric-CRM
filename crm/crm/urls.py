0039e72b9d147a5b65cad2a4db58799e7d2f9c9e
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-12 18:51:38 +0300

    Signup page

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import index, about
+from userprofile.views import signup
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('signup/', signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
--- crm/userprofile/views.py
+++ crm/userprofile/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm #Will contain info on username, email and password
+from django.shortcuts import render, redirect
+from .models import UserProfile
 
 # Create your views here.
+def signup(request): 
+    #checking if the form has been submitted
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        #checking if the info is correct before saving it to the db
+        if form.is_valid():
+            user = form.save()
+            UserProfile.objects.create(user=user) #creating the user profile
+
+            #redirecting to the login page
+            return redirect('/login/')
+    else: 
+        form = UserCreationForm()
+    
+    return render(request, 'userprofile/signup.html', {'form': form})
