69b3514dc45dc4638d6c1ae2c67540deba842191
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-14 14:51:21 +0300

    Listing out the Leads

--- crm/lead/views.py
+++ crm/lead/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import AddLeadForm
 from django.shortcuts import redirect
+from .models import Lead
 
 # Create your views here.
+@login_required
+def leads_list(request):
+    leads = Lead.objects.filter(created_by=request.user)
+
+    return render(request, 'lead/leads_list.html', {
+        'leads': leads
+    })
+
 @login_required
 def add_lead(request):
     if request.method == 'POST':
@@ -16,7 +25,7 @@ def add_lead(request):
             return redirect('dashboard')
     else:
          form = AddLeadForm()
-         
+
     return render(request, 'lead/add_lead.html', {
         'form': form
     })
