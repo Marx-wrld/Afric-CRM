1a1ba5c4bd253d7cadf78bc091987f023057be91
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-14 16:21:12 +0300

    Edit leads functionality

--- crm/lead/urls.py
+++ crm/lead/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.leads_list, name='leads_list'),
     path('<int:pk>/', views.leads_detail, name='leads_detail'),
     path('<int:pk>/delete/', views.leads_delete, name='leads_delete'),
+    path('<int:pk>/edit/', views.leads_edit, name='leads_edit'),
     path('add-lead/', views.add_lead, name='add_lead'),
 ]
--- crm/lead/views.py
+++ crm/lead/views.py
@@ -32,6 +32,26 @@ def leads_delete(request, pk):
     return redirect('leads_list')
 
 @login_required
+def leads_edit(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+        
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Lead updated successfully')
+
+            return redirect('leads_list')
+    else:
+        form = AddLeadForm(instance=lead)
+    
+    return render(request, 'lead/leads_edit.html', {
+        'form': form
+    })
+
+@login_required
 def add_lead(request):
     if request.method == 'POST':
         form = AddLeadForm(request.POST)
