a7c33c512964a801726aebd79c34426c320b1992
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-14 18:17:32 +0300

    Showing client details

--- crm/client/urls.py
+++ crm/client/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.clients_list, name='clients_list'),
+    path('<int:pk>/', views.clients_detail, name='clients_detail'),
 ]
--- crm/client/views.py
+++ crm/client/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Client
 from django.contrib.auth.decorators import login_required
 
@@ -7,6 +7,15 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def clients_list(request):
     clients = Client.objects.filter(created_by=request.user)
+
     return render(request, 'client/clients_list.html', {
         'clients': clients
+    })
+
+@login_required
+def clients_detail(request, pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    return render(request, 'client/clients_detail.html', {
+        'client': client
     })
