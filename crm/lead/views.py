df0a45560a242a33da8483b246342a4e2ecd5496
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-14 17:38:03 +0300

    Leads to client conversion functionality

--- crm/lead/views.py
+++ crm/lead/views.py
@@ -4,11 +4,12 @@ from .forms import AddLeadForm
 from django.shortcuts import redirect, get_object_or_404
 from .models import Lead
 from django.contrib import messages
+from client.models import Client
 
 # Create your views here.
 @login_required
 def leads_list(request):
-    leads = Lead.objects.filter(created_by=request.user)
+    leads = Lead.objects.filter(created_by=request.user, converted_to_client=False)
 
     return render(request, 'lead/leads_list.html', {
         'leads': leads
@@ -69,3 +70,22 @@ def add_lead(request):
     return render(request, 'lead/add_lead.html', {
         'form': form
     })
+
+@login_required
+def convert_to_client(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    
+    client = Client.objects.create(
+        name=lead.name,
+        email=lead.email,
+        description=lead.description,
+        created_by=request.user
+
+    )
+
+    lead.converted_to_client = True
+    lead.save()
+
+    messages.success(request, 'Lead converted to client successfully')
+  
+    return redirect('leads_list')
