14e792e65b821ba5270c058554672ec7aae5dbb8
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-14 18:03:37 +0300

    Functionality to show the list of clients

--- crm/client/views.py
+++ crm/client/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Client
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def clients_list(request):
+    clients = Client.objects.filter(created_by=request.user)
+    return render(request, 'client/clients_list.html', {
+        'clients': clients
+    })
