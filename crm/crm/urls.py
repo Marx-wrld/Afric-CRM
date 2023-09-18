19541812e8bd1b40a6cec7996d6398f0a3c7bbf4
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-18 17:11:48 +0300

    Team owner/admin to be able to edit team name

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     path('dashboard/clients/', include('client.urls')),
     path('dashboard/leads/', include('lead.urls')),
     path('dashboard/', include('dashboard.urls')), # All urls that begin with dashboard will be handled by dashboard/urls.py
+    path('dashboard/myaccount/', myaccount, name='myaccount'),
+    path('dashboard/teams/', include('team.urls')),
     path('about/', about, name='about'),
     path('signup/', signup, name='signup'),
     path('login/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
-    path('dashboard/myaccount/', myaccount, name='myaccount'),
     path('logout', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
