088cb0ff6606b6a395367e0b5177a264128c9e62
Marx-wrld <iongonga@kabarak.ac.ke>
2023-09-28 17:57:59 +0300

    Bug fix - NoReverseMatch at /about/ Reverse for 'detail' with arguments '('',)' not found

--- crm/team/context_processors.py
+++ crm/team/context_processors.py
@@ -1,6 +1,12 @@
 from .models import Team
 
 def team(request):
+    if request.user.is_authenticated:
+        return {
+            'team': Team.objects.filter(members=request.user)[0]
+        }
+    else:
+        team = None
 
     return {
         'team': Team.objects.all()
