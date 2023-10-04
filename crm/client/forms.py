9425f145df08a55f6d30ee720ae62b07ac4897bc
Marx-wrld <iongonga@kabarak.ac.ke>
2023-10-04 09:28:50 +0300

    Lead and Client form styling

--- crm/client/forms.py
+++ crm/client/forms.py
@@ -6,12 +6,44 @@ class AddClientForm(forms.ModelForm):
         model = Client
         fields = ('name', 'email', 'description')
 
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-6 my-4 w-full py-4 px-6 rounded-xl bg-gray-100 border border-gray-200 placeholder-gray-500 focus:outline-none focus:border-gray-400', 
+            'placeholder': 'Name'
+            })
+        )
+    email = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-6 my-4 w-full py-4 px-6 rounded-xl bg-gray-100 border border-gray-200 placeholder-gray-500 focus:outline-none focus:border-gray-400', 
+            'placeholder': 'Email'
+            })
+        )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control mb-6 my-4 w-full py-4 px-6 rounded-xl bg-gray-100 border border-gray-200 placeholder-gray-500 focus:outline-none focus:border-gray-400', 
+            'placeholder': 'Description'
+            })
+        )
+
 class AddCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+    
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control mb-6 my-4 w-full py-4 px-6 rounded-xl bg-gray-100 border border-gray-200 placeholder-gray-500 focus:outline-none focus:border-gray-400', 
+            'placeholder': 'Comment'
+            })
+        )
 
 class AddFileForm(forms.ModelForm):
     class Meta:
         model = ClientFile
         fields = ('file',)
+    
+    file = forms.CharField(
+        widget=forms.FileInput(attrs={
+            'class': 'form-control mb-6 my-4 w-full py-4 px-6 rounded-xl bg-gray-100 border border-gray-200 placeholder-gray-500 focus:outline-none focus:border-gray-400'
+            })
+        )
